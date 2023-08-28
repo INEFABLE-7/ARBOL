be36fc427ae2a64fa5cead4696071a873c04ee42
INEFABLE-7 <1460446@senati.pe>
2023-08-27 22:04:02 -0500

    iniciamos con una clase que representa un nodo del árbol de decisión.

---
+++ ArbolDecisionID3/src/main/java/com/mycompany/arboldecisionid3/ArbolDecisionID3.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.arboldecisionid3;
+package arboldecisionid3;
+
+import java.util.*;
+
+class NodoArbol {
+    String atributo;
+    Map<String, NodoArbol> hijos;
+    String resultado;
+
+    NodoArbol(String atributo) {
+        this.atributo = atributo;
+        hijos = new HashMap<>();
+        resultado = null;
+    }
+}
+/**
+ *
+ * @author JEAN
+ */
+public class ArbolDecisionID3 {
+
+    public static void main(String[] args) {
+     
+        
+    }
+}
+
+
+
