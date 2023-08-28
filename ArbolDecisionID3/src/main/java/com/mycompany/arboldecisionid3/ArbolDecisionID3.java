6566c14aa285b6f6465efdf22148dd6eff504b9e
INEFABLE-7 <1460446@senati.pe>
2023-08-27 22:08:49 -0500

    despues agregamos los datos de entrenamiento y se construimos el árbol de decisión.

--- ArbolDecisionID3/src/main/java/com/mycompany/arboldecisionid3/ArbolDecisionID3.java
+++ ArbolDecisionID3/src/main/java/com/mycompany/arboldecisionid3/ArbolDecisionID3.java
@@ -7,6 +7,9 @@ package arboldecisionid3;
 
 import java.util.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class NodoArbol {
     String atributo;
     Map<String, NodoArbol> hijos;
@@ -25,6 +28,14 @@ class NodoArbol {
 public class ArbolDecisionID3 {
 
     public static void main(String[] args) {
+         List<String[]> datos = new ArrayList<>();
+        datos.add(new String[]{"Soleado", "Caluroso", "Alta", "No"});
+        datos.add(new String[]{"Soleado", "Caluroso", "Normal", "No"});
+        datos.add(new String[]{"Nublado", "Templado", "Alta", "Sí"});
+        datos.add(new String[]{"Lluvioso", "Fresco", "Alta", "Sí"});
+        datos.add(new String[]{"Lluvioso", "Fresco", "Normal", "Sí"});
+        datos.add(new String[]{"Nublado", "Fresco", "Normal", "Sí"});
+        datos.add(new String[]{"Soleado", "Templado", "Alta", "No"});
      
         
     }
